09bfc5217d7528f81c50f321d1203def01f14fa4
PaulPruenster <paul.pruenster@gmail.com>
2024-01-17 19:33:20 +0100

    dynamo update

--- lambdas/dynamoGet.py
+++ lambdas/dynamoGet.py
@@ -1,11 +1,14 @@
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 
+# example request body:
+# {}
+
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     parking_table = dynamodb.Table('parking')
 
-    items = parking_table.scan(FilterExpression=Attr('is_empty').eq(True))
+    items = parking_table.scan(FilterExpression=Attr('is_empty').eq('true'))
 
     return {
         'statusCode': 200,
---
+++ lambdas/dynamoUpdate.py
@@ -0,0 +1,35 @@
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+# example request body:
+# {
+#   "id": 0,  
+#   "is_empty": "true"
+# }
+
+def lambda_handler(event, context):
+    # Get the DynamoDB client
+    dynamodb = boto3.resource('dynamodb')
+
+    # Get the parking table
+    parking_table = dynamodb.Table('parking')
+
+    # Get the id of the item to insert
+    id = event['id']
+    is_empty = event['is_empty']
+
+    response = parking_table.update_item(
+    Key={'parking_id': id},
+    UpdateExpression="set is_empty = :r",
+    ExpressionAttributeValues={
+        ':r': is_empty,
+    },
+    ReturnValues="UPDATED_NEW"
+)
+
+    # Return a success response
+    return {
+        'statusCode': 200,
+        'body': 'Item inserted successfully'
+    }
+
