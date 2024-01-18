d898cda264717857b5c3aebaef43506666bf0056
lukas-niederstaetter <lukasniederstatter@gmail.com>
2024-01-18 18:32:31 +0100

    updated und jetz passts

--- lambdas/sendFireSMS.py
+++ lambdas/sendFireSMS.py
@@ -6,7 +6,7 @@ phone_number = "+393409590100"
 
 sns = boto3.client('sns', region_name=aws_region)
 
-def send_sms(id):
+def lambda_handler(event, context):
     sns.publish(
         PhoneNumber=phone_number,
         Message='Caution: Fire detected in parking space {}'.format(id)
