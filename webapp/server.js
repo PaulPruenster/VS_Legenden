9c58d6e6a3aa1289912facd9e01c483dd4768aed
PaulPruenster <paul.pruenster@gmail.com>
2024-01-19 13:34:37 +0100

    server script

---
+++ webapp/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/frontend.html');
+});
+
+app.get('/backend', (req, res) => {
+    res.sendFile(__dirname + '/backend.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
