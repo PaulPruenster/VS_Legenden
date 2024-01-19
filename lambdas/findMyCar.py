f28d7571b4a5bbf4ecea8ea230f110be3d58325e
Matthias Ebner <csaz9531@student.uibk.ac.at>
2024-01-19 21:31:03 +0100

    last try

--- lambdas/findMyCar.py
+++ lambdas/findMyCar.py
@@ -1,26 +1,56 @@
 import boto3
+import json
 from boto3.dynamodb.conditions import Key, Attr
 
+  
 def lambda_handler(event, context):
-    # Get the input string from the event
-    number_plate = event['number_plate']
+    print("start")
+    print(event)
+    if 'body' not in event:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": "no body was send."
+            })
+        }
+
+    # load json data from body
+    body = json.loads(event['body'])
+
+    if 'number_plate' not in body:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": "number_plate or image missing."
+            })
+        }
+
+    number_plate = body['number_plate']
+    print(number_plate)
 
     dynamodb = boto3.resource('dynamodb')
     parking_table = dynamodb.Table('parking')
 
-    items = parking_table.scan(FilterExpression=Attr('number_plate').contains(number_plate))
-
+    items = parking_table.scan(FilterExpression=Attr(
+        'number_plate').contains(number_plate))
     if len(items['Items']) == 1:
-        return {
-            'statusCode': 200,
-            'body': {
-                'items': items['Items']
-            }
+        print(items['Items'][0])
+        items['Items'][0]['parking_id'] = int(items['Items'][0]['parking_id'])
+        response = {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "car was found",
+                "item": items['Items'][0]
+            })
         }
-        
+
+        print(response)
+
+        return response
+
     return {
-            'statusCode': 404,
-            'body': {
-                'error': 'No or more than one car was found'
-            }
-        }
+        "statusCode": 400,
+        "body": json.dumps({
+            "message": "No or more than one car was found"
+        })
+    }
--- lambdas/getParkingSpace.py
+++ lambdas/getParkingSpace.py
@@ -1,20 +1,26 @@
 import boto3
+import json
 from boto3.dynamodb.conditions import Key, Attr
 
 # example request body:
-# {
-#   "number_plate": "test123"
-# }
+# {}
+
 
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     parking_table = dynamodb.Table('parking')
-    
-    number_plate = event['number_plate']
 
-    items = parking_table.scan(FilterExpression=Attr('number_plate').eq(number_plate))
+    items = parking_table.scan(FilterExpression=Attr('is_empty').eq(True))
+    for i in range(len(items['Items'])):
+        items['Items'][i]['parking_id'] = int(items['Items'][i]['parking_id'])
+    print(items)
 
-    return {
+    # Create the response payload
+    response = {
         'statusCode': 200,
-        'body': items['Items'][0]['parking_id']
+        'body': json.dumps({
+            'items': items['Items']
+        })
     }
+
+    return response
