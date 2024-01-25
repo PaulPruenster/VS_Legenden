b933cc6d23a9870d27ed20656fd43a31b94b5c42
PaulPruenster <paul.pruenster@gmail.com>
2024-01-25 16:30:58 +0100

    maerial design

--- webapp/server.js
+++ webapp/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
-
+80
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/frontend.html');
 });
